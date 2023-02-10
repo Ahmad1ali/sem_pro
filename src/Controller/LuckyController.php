e2923bc78e1dd7db14528582ba9c7521a5ca30ab
Ahmad1ali <302516885@student.rocmondriaan.nl>
2023-02-10 10:25:27 +0100

    luckycontroller gemaakt

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+class LuckyController extends AbstractController
+{
+
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        return $this->render('bezoker/number.html.twig', ['number'=>$number]);
+        return new Response(
+//            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+
+
+//    #[Route('/goedemorgen')]
+//     public function morgenMeneerjong(){
+//    }
+}
