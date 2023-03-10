8598d89c1e2be48f1e3b22310e7223ed41e3ac3e
Ahmad1ali <92579960+Ahmad1ali@users.noreply.github.com>
2023-03-10 10:17:42 +0100

    task form symfony

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Task;
+use App\Form\TaskType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TaskController extends AbstractController
+{
+    #[Route('/taskform')]
+    public function new(Request $request): Response
+    {
+        $task = new Task();
+        // ...
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        return $this->renderForm('task/taskform.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $naam = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $Datum = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNaam(): ?string
+    {
+        return $this->naam;
+    }
+
+    public function setNaam(string $naam): self
+    {
+        $this->naam = $naam;
+
+        return $this;
+    }
+
+    public function getDatum(): ?\DateTimeInterface
+    {
+        return $this->Datum;
+    }
+
+    public function setDatum(\DateTimeInterface $Datum): self
+    {
+        $this->Datum = $Datum;
+
+        return $this;
+    }
+}
