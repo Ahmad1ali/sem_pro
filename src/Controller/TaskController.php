b26d23e60728124def18592ad885f1f9b5e5191c
Ahmad1ali <92579960+Ahmad1ali@users.noreply.github.com>
2023-03-14 12:54:08 +0100

    requist form done

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 use App\Entity\Task;
 use App\Form\TaskType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -13,16 +15,31 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
-    #[Route('/taskform')]
-    public function new(Request $request): Response
+
+    #[Route('/taskform',name: 'app_task')]
+    public function new(Request $request,EntityManagerInterface $entityManager): Response
     {
         $task = new Task();
         // ...
+//       $managerRegistry = $doctrine->getRepository( Task::class)->findAll();
 
         $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_task');
+        }
 
         return $this->renderForm('task/taskform.html.twig', [
             'form' => $form,
+//            'show'=>$ManagerRegistryr,
         ]);
     }
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -5,15 +5,22 @@ namespace App\Form;
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('naam')
-            ->add('Datum')
+            ->add('naam',TextType::class)
+            ->add('Datum', DateType::class)
+            ->add('save', SubmitType::class)
+
+
         ;
     }
 
